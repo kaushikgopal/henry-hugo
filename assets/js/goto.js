c975831d6341046c82d7811216179a51d049d469
kaush <1042037+kaushikgopal@users.noreply.github.com>
2022-07-22 16:35:41 -0700

    goto.js: change functionality to show both buttons only when scrolling

--- assets/js/goto.js
+++ assets/js/goto.js
@@ -5,58 +5,83 @@
  *
  **/
 
+/**
+ * Helper jquery function to detect scrolling
+ */
 (function () {
-  $(window).on("load", function () {
-    const $gotoBottomBtn = $(".goto.bottom"),
-      $gotoTopBtn = $(".goto.top"),
-      SCROLLED_TOP_BY = 50,
-      SCROLLED_TO_BOTTOM_SECTION =
-        $(document).height() - ($(this).height() + $("#site-footer").height());
+  var lastScrollAt = Date.now(),
+    timeout;
 
-    // console.log(" ---> goto loading [SCROLLED_TO_BOTTOM_SECTION: " + SCROLLED_TO_BOTTOM_SECTION + "]");
+  function scrollStartStop() {
+    var $this = $(this);
 
-    // first show the buttons through CSS
-    // opacity also will need to be set
-    $gotoBottomBtn.add($gotoTopBtn).css({ display: "block" });
+    if (Date.now() - lastScrollAt > 100) $this.trigger("scrollstart");
 
-    let scrollPosition = 0;
+    lastScrollAt = Date.now();
 
-    $(this).scroll(function () {
-      scrollPosition = $(this).scrollTop();
-      // console.log(" ---> now at "+ scrollPosition);
-      showOrHideGotoBtns(scrollPosition);
+    clearTimeout(timeout);
+
+    timeout = setTimeout(function () {
+      if (Date.now() - lastScrollAt > 99) $this.trigger("scrollend");
+    }, 100);
+  }
+
+  $(document).on("scroll", scrollStartStop);
+})();
+
+/**
+ * calls the function func once within the within time window.
+ * this is a debounce function which actually calls the func as
+ * opposed to returning a function that would call func.
+ *
+ * @param func    the function to call
+ * @param within  the time window in milliseconds, defaults to 300
+ * @param timerId an optional key, defaults to func
+ */
+function callOnce(func, within = 300, timerId = null) {
+  window.callOnceTimers = window.callOnceTimers || {};
+  if (timerId == null) timerId = func;
+  var timer = window.callOnceTimers[timerId];
+  clearTimeout(timer);
+  timer = setTimeout(() => func(), within);
+  window.callOnceTimers[timerId] = timer;
+}
+
+(function () {
+
+  function showBtn($btn) {
+    $btn.css({
+      opacity: "0.75",
+      filter: "alpha(opacity=75)",
+      "-moz-opacity": "0.75",
     });
+  }
 
-    function showOrHideGotoBtns(scrollPosition) {
-      if (
-        scrollPosition > SCROLLED_TOP_BY &&
-        scrollPosition < SCROLLED_TO_BOTTOM_SECTION
-      ) {
-        showBtn($gotoBottomBtn);
-      } else {
+  function hideBtn($btn) {
+    $btn.css({
+      opacity: "0",
+      filter: "alpha(opacity=0)",
+      "-moz-opacity": "0",
+    });
+  }
+
+  const
+    $gotoBottomBtn = $(".goto.bottom"),
+    $gotoTopBtn = $(".goto.top");
+
+  // first show the buttons through CSS
+  // opacity also will need to be set
+  $gotoBottomBtn.add($gotoTopBtn).css({ display: "block" });
+
+  $(document).on("scrollstart", function () {
+    showBtn($gotoBottomBtn);
+    showBtn($gotoTopBtn);
+  });
+
+  $(document).on("scrollend", function () {
+    callOnce(function () {
         hideBtn($gotoBottomBtn);
-      }
-
-      if (scrollPosition >= SCROLLED_TO_BOTTOM_SECTION) {
-        showBtn($gotoTopBtn);
-      } else {
         hideBtn($gotoTopBtn);
-      }
-    }
-
-    function showBtn($btn) {
-      $btn.css({
-        opacity: "0.75",
-        filter: "alpha(opacity=75)",
-        "-moz-opacity": "0.75",
-      });
-    }
-    function hideBtn($btn) {
-      $btn.css({
-        opacity: "0",
-        filter: "alpha(opacity=0)",
-        "-moz-opacity": "0",
-      });
-    }
+    });
   });
 })();
