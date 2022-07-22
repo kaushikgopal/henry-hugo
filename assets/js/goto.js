51a6da63ab5acc3610bd84f3044b62e1a3532a7c
kaush <1042037+kaushikgopal@users.noreply.github.com>
2022-07-22 15:09:35 -0700

    goto.js format

--- assets/js/goto.js
+++ assets/js/goto.js
@@ -6,60 +6,57 @@
  **/
 
 (function () {
-    $(window).on("load", function () {
+  $(window).on("load", function () {
+    const $gotoBottomBtn = $(".goto.bottom"),
+      $gotoTopBtn = $(".goto.top"),
+      SCROLLED_TOP_BY = 50,
+      SCROLLED_TO_BOTTOM_SECTION =
+        $(document).height() - ($(this).height() + $("#site-footer").height());
 
-        const   $gotoBottomBtn = $('.goto.bottom'),
-                $gotoTopBtn = $('.goto.top'),
-                SCROLLED_TOP_BY = 50,
-                SCROLLED_TO_BOTTOM_SECTION  = $(document).height() - ($(this).height() + $('#site-footer').height());
+    // console.log(" ---> goto loading [SCROLLED_TO_BOTTOM_SECTION: " + SCROLLED_TO_BOTTOM_SECTION + "]");
 
-        // console.log(" ---> goto loading [SCROLLED_TO_BOTTOM_SECTION: " + SCROLLED_TO_BOTTOM_SECTION + "]");
+    // first show the buttons through CSS
+    // opacity also will need to be set
+    $gotoBottomBtn.add($gotoTopBtn).css({ display: "block" });
 
-        // first show the buttons through CSS
-        // opacity also will need to be set
-        $gotoBottomBtn
-            .add($gotoTopBtn)
-            .css({ display: "block" });
+    let scrollPosition = 0;
 
-
-        let scrollPosition = 0;
-
-        $(this).scroll(function() {
-            scrollPosition = $(this).scrollTop();
-            // console.log(" ---> now at "+ scrollPosition);
-            showOrHideGotoBtns(scrollPosition)
-        });
-
-
-        function showOrHideGotoBtns(scrollPosition) {
-            if(scrollPosition > SCROLLED_TOP_BY && scrollPosition < SCROLLED_TO_BOTTOM_SECTION) {
-                showBtn($gotoBottomBtn);
-            } else {
-                hideBtn($gotoBottomBtn);
-            }
-
-            if(scrollPosition >= SCROLLED_TO_BOTTOM_SECTION) {
-                showBtn($gotoTopBtn);
-            } else {
-                hideBtn($gotoTopBtn);
-            }
-        }
-
-        function showBtn($btn ) {
-            $btn.css({
-                opacity: "0.75",
-                'filter':'alpha(opacity=75)',
-                '-moz-opacity': "0.75"
-            });
-        }
-        function hideBtn($btn ) {
-            $btn.css({
-                opacity: "0",
-                'filter':'alpha(opacity=0)',
-                '-moz-opacity': "0"
-            });
-        }
-
+    $(this).scroll(function () {
+      scrollPosition = $(this).scrollTop();
+      // console.log(" ---> now at "+ scrollPosition);
+      showOrHideGotoBtns(scrollPosition);
     });
 
+    function showOrHideGotoBtns(scrollPosition) {
+      if (
+        scrollPosition > SCROLLED_TOP_BY &&
+        scrollPosition < SCROLLED_TO_BOTTOM_SECTION
+      ) {
+        showBtn($gotoBottomBtn);
+      } else {
+        hideBtn($gotoBottomBtn);
+      }
+
+      if (scrollPosition >= SCROLLED_TO_BOTTOM_SECTION) {
+        showBtn($gotoTopBtn);
+      } else {
+        hideBtn($gotoTopBtn);
+      }
+    }
+
+    function showBtn($btn) {
+      $btn.css({
+        opacity: "0.75",
+        filter: "alpha(opacity=75)",
+        "-moz-opacity": "0.75",
+      });
+    }
+    function hideBtn($btn) {
+      $btn.css({
+        opacity: "0",
+        filter: "alpha(opacity=0)",
+        "-moz-opacity": "0",
+      });
+    }
+  });
 })();
